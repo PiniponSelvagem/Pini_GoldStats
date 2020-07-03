67e67f44fe84c185f0e8353e801a2be163c78d01
PiniponSelvagem <piniponselvagem@gmail.com>
2020-07-03 22:10:44 +0100

    Added ElvUI datatext for session gold/h. Improved main code.

--- Pini_GoldStats.lua
+++ Pini_GoldStats.lua
@@ -1,11 +1,13 @@
 ﻿-- Author      : PiniponSelvagem
 -- Create Date : 2019/06/30
--- Updated     : 2019/10/02
+-- Updated     : 2020/06/23
 -- Based of    : xp_timer by jjacob (Jeff Jacob)
 
 -- ENABLE lua debug errors: /console scriptErrors 1
 
-local pgs = {};
+
+PiniGS = {} -- exported functions in this object
+local pgs = {}
 local colors = {
     --|cAARRGGBB |r
     RED     = "|cFFFF0000",
@@ -26,7 +28,7 @@ local session = {
 }
 
 local instance = {
-    valid = false,
+    isValid = false,
     inProgress = false,
     wasShownOnLeave = true, -- starts at true to not show instance stats on login
     name = "N/A",
@@ -46,12 +48,16 @@ local strings = {
         RESET = colors.YELLOW .. "Pini GoldStats reseted and ready|r",
     },
     ["help"] = {
-        colors.YELLOW .. "Pini GoldStats usage:|r",
+        colors.YELLOW .. "Pini GoldStats:|r",
+        colors.RED    .. " ----- |rCommands" .. colors.RED .. " -----|r",
         colors.YELLOW .. " /pgs|r -- Get information about gold earned",
         colors.YELLOW .. " /pgs help|r -- this help",
         --colors.YELLOW .. " /pgs hour|r -- gold earned last hour",
         colors.YELLOW .. " /pgs instance|r -- last instance run gold stats",
         colors.YELLOW .. " /pgs reset|r -- reset your gold stats",
+        colors.RED    .. " --- |rElvUI datatext" .. colors.RED .. " ---|r",
+        colors.GREEN  .. " SET one at:|r " .. colors.CYAN .. "ElvUI|r > DataTexts > Panels",
+        colors.YELLOW .. " PGS - session gold/h|r -- show session gold per hour"
     },
     ["session"] = {
         title = colors.YELLOW .. "Pini GoldStats current session:|r",
@@ -70,11 +76,11 @@ local strings = {
 }
 
 
-local pgs_frame = CreateFrame("Frame");
-pgs_frame:RegisterEvent("ADDON_LOADED");
-pgs_frame:RegisterEvent("PLAYER_LOGIN");
-pgs_frame:RegisterEvent("PLAYER_MONEY");
-pgs_frame:RegisterEvent("PLAYER_ENTERING_WORLD");
+local pgs_frame = CreateFrame("Frame")
+pgs_frame:RegisterEvent("ADDON_LOADED")
+pgs_frame:RegisterEvent("PLAYER_LOGIN")
+--pgs_frame:RegisterEvent("PLAYER_MONEY")
+pgs_frame:RegisterEvent("PLAYER_ENTERING_WORLD")
 
 pgs_frame:SetScript("OnEvent",
     function(self,event,...)
@@ -83,6 +89,48 @@ pgs_frame:SetScript("OnEvent",
         end
     end
 )
+
+
+
+--[[
+    ---------------------
+        AUX functions
+    ---------------------
+]]
+
+local function getSessionTime()
+    return GetTime() - session.startTime
+end
+local function getSessionMoney()
+    return GetMoney() - session.startMoney
+end
+
+local function getInstanceTime()
+    return instance["time"].endT - instance["time"].startT
+end
+local function getInstanceMoney()
+    return instance["money"].endM - instance["money"].startM
+end
+
+local function tryUpdateInstanceInfo()
+    if instance.inProgress then
+        instance["time"].endT  = GetTime()
+        instance["money"].endM = GetMoney()
+    end
+end
+local function setNewInstanceInfo()
+    instance["time"].startT  = GetTime();
+    instance["money"].startM = GetMoney();
+end
+
+local function reset()
+    session.startMoney = GetMoney()
+    session.startTime  = GetTime()
+end
+
+local function printToChat(...)
+    DEFAULT_CHAT_FRAME:AddMessage(string.format(...))
+end
 
 --[[
 Usage:
@@ -96,107 +144,100 @@ local function arrayValues(t)
 end
 
 local function secondsToHMS(seconds)
-    hours   = math.floor(seconds / 3600);
-    seconds = seconds - (hours * 3600);
-    minutes = math.floor(seconds / 60);
-    seconds = math.floor(seconds - (minutes * 60));
-    return hours,minutes,seconds;
+    hours   = math.floor(seconds / 3600)
+    seconds = seconds - (hours * 3600)
+    minutes = math.floor(seconds / 60)
+    seconds = math.floor(seconds - (minutes * 60))
+    return hours,minutes,seconds
 end
 local function hmsToString(seconds)
-    return string.format("%d:%.2d:%.2d", secondsToHMS(seconds));
+    return string.format("%d:%.2d:%.2d", secondsToHMS(seconds))
 end --to_hms_string
 
 local function moneyToGSC(copper)
-    local positive = 1;
+    local positive = 1
     if copper < 0 then
-        positive = -1;
+        positive = -1
     end
-    copper = copper * positive;
-    gold = math.floor(copper / 10000);
-    copper = copper - (gold * 10000);
-    silver = math.floor(copper / 100);
-    copper = math.floor(copper - (silver * 100));
-    return gold,silver,copper;
+    copper = copper * positive
+    gold = math.floor(copper / 10000)
+    copper = copper - (gold * 10000)
+    silver = math.floor(copper / 100)
+    copper = math.floor(copper - (silver * 100))
+    return gold,silver,copper
 end
 local function gscToString(copper)
-    local ret = string.format("%d" .. colors.GOLD .. "g|r %d" .. colors.SILVER .. "s|r %d" .. colors.COPPER .."c|r", moneyToGSC(copper));
+    local ret = string.format("%d" .. colors.GOLD .. "g|r %d" .. colors.SILVER .. "s|r %d" .. colors.COPPER .."c|r", moneyToGSC(copper))
     if (copper < 0) then
-        ret = "-" .. ret;
+        ret = "-" .. ret
     end
-    return ret;
+    return ret
 end
 
 local function moneyPerHour(money, seconds)
-    return (money / seconds) * 3600;
+    if seconds > 0 then
+        return (money / seconds) * 3600
+    end
+    return 0
 end
+
+
+
+function pgs:handleSlashes(msg)
+    local command, rest = msg:match("^(%S*)%s*(.-)$")
+    if command == "" then
+        self:default()
+    else
+        if self[command] and type(self[command]) == "function"  then
+            return self[command](self,rest)
+        else
+            self:help(msg)
+        end
+    end
+end
+
+--[[
+    This is called X time after entering an instance, since for some reason
+    difficultyName gets its update delayed by the WoW client.
+    It can be called by the player/you, either if you managed to type it by luck
+    or came here and check the source code xD.
+    No problem in calling this command outside of an instance.
+]]
+function pgs:updateInstaceDifficultyName()
+    if IsInInstance() then
+        local name, type, difficultyIndex, difficultyName, maxPlayers,
+            dynamicDifficulty, isDynamic, instanceMapId, lfgID = GetInstanceInfo()
+        
+        if difficultyName == "" then
+            difficultyName = "N/A"
+        end
+        instance.difficultyName = difficultyName
+    end
+end
+
+--[[
+    ---------------------
+       Event functions
+    ---------------------
+]]
 
 function pgs:ADDON_LOADED(...)
     local addon = ...
     if addon == "Pini_GoldStats" then
         SlashCmdList["PINIGOLDSTATS"] = function(msg)
-            pgs:handleSlashes(msg);
+            pgs:handleSlashes(msg)
         end -- end function
-        SLASH_PINIGOLDSTATS1 = "/pgs";
-        SLASH_PINIGOLDSTATS2 = "/pini_goldstats";
+        SLASH_PINIGOLDSTATS1 = "/pgs"
+        SLASH_PINIGOLDSTATS2 = "/pini_goldstats"
     end
 end
 
 function pgs:PLAYER_LOGIN(...)
-    --self:cash_timer_setup()
     self:reset();
-end -- TODO
-
-function pgs:handleSlashes(msg)
-    local command, rest = msg:match("^(%S*)%s*(.-)$");
-    --DEFAULT_CHAT_FRAME:AddMessage("^"..command.."^");
-    --DEFAULT_CHAT_FRAME:AddMessage(rest);
-    if command == "" then
-        self:default();
-    --elseif command == "on" or command == "off" then
-    --    self:ctdefault(msg);
-    else
-        if self[command] and type(self[command]) == "function"  then
-            return self[command](self,rest)
-        else
-            self:help(msg);
-        end
-    end
-end
-
-function pgs:default()
-    local sessionTime  = GetTime()  - session.startTime;
-    local sessionMoney = GetMoney() - session.startMoney;
-    DEFAULT_CHAT_FRAME:AddMessage(strings.session.title);
-    DEFAULT_CHAT_FRAME:AddMessage(string.format(strings.session.sessionTime, hmsToString(sessionTime)));
-    DEFAULT_CHAT_FRAME:AddMessage(string.format(
-            strings.session.moneyTotal,
-            gscToString(sessionMoney),
-            gscToString(moneyPerHour(sessionMoney, sessionTime))
-        )
-    );
-end
-
-function pgs:help(msg)
-    for v in arrayValues(strings["help"]) do
-        DEFAULT_CHAT_FRAME:AddMessage(v);
-    end
 end
 
 --[[
-function pgs:hour()
-    local time_diff = GetTime() - self.start_time;
-    if time_diff >= (3600) then
-        local xp_per_hour = (self.xp_gained / time_diff) * 3600;
-        DEFAULT_CHAT_FRAME:AddMessage("XP per hour: "..xp_per_hour);
-    else
-        DEFAULT_CHAT_FRAME:AddMessage("You have not been logged in without a reset for an hour");
-        DEFAULT_CHAT_FRAME:AddMessage("Time Logged IN: "..xp_util.to_hms_string(time_diff));
-    end
-end --TODO
-]]
-
 function pgs:PLAYER_MONEY(...)
-    --[[
     local current_time = math.floor(GetTime());
     --DEFAULT_CHAT_FRAME:AddMessage("You where paid at "..xp_util.to_hms_string(current_time));
     local cash_time_diff = current_time - self.cash_time_last_paid ;
@@ -220,69 +261,135 @@ function pgs:PLAYER_MONEY(...)
         xpt_character_data.cash_values_array[xpt_character_data.cash_running_time] = xpt_character_data.cash_values_array[xpt_character_data.cash_running_time]+ cash_diff;
     end
     --DEFAULT_CHAT_FRAME:AddMessage("You recieved "..xp_util.to_gsc_string(xpt_character_data.cash_values_array[xpt_character_data.cash_running_time]).."this second.");
-    ]]
 end -- TODO
+]]
 
 function pgs:PLAYER_ENTERING_WORLD(...)
-    --self:cash_timer_setup()
     if IsInInstance() then
         local name, type, difficultyIndex, difficultyName, maxPlayers,
-            dynamicDifficulty, isDynamic, instanceMapId, lfgID = GetInstanceInfo();
+            dynamicDifficulty, isDynamic, instanceMapId, lfgID = GetInstanceInfo()
 
-        instance.valid = true;
-        instance.inProgress = true;
-        instance.wasShownOnLeave = false;
-        instance.name = name;
-        instance.difficultyName  = difficultyName;
-        instance["time"].startT  = GetTime();
-        instance["money"].startM = GetMoney();
+        C_Timer.After(1, pgs.updateInstaceDifficultyName)
+
+        instance.isValid = true
+        instance.inProgress = true
+        instance.wasShownOnLeave = false
+        instance.name = name
+        instance.difficultyName = difficultyName
+        setNewInstanceInfo()
     else
         if instance.wasShownOnLeave == false then
-            instance.inProgress = false;
-            instance["time"].endT  = GetTime();
-            instance["money"].endM = GetMoney();
-            pgs:instance();
-            instance.wasShownOnLeave = true;
+            tryUpdateInstanceInfo()
+            instance.inProgress = false
+            pgs:instance()
+            instance.wasShownOnLeave = true
         end
     end
 end
 
-function pgs:instance()
-    local instanceTitle;
-    if instance.inProgress then
-        instanceTitle = string.format(strings.instance.title,
-                instance.name,
-                instance.difficultyName,
-                strings.instance.inProgress
-        );
-        instance["time"].endT  = GetTime();
-        instance["money"].endM = GetMoney();
-    else
-        instanceTitle = string.format(strings.instance.title,
-                instance.name,
-                instance.difficultyName,
-                strings.instance.done
-        );
-    end
 
-    if instance.valid then
-        local moneyTotal = instance["money"].endM - instance["money"].startM;
-        local timeTotal  = instance["time"].endT - instance["time"].startT;
-        DEFAULT_CHAT_FRAME:AddMessage(instanceTitle);
-        DEFAULT_CHAT_FRAME:AddMessage(
-                string.format(strings["instance"].info,
-                    hmsToString(timeTotal),
-                    gscToString(moneyTotal),
-                    gscToString(moneyPerHour(moneyTotal, timeTotal))
-                )
-        );
+
+--[[
+    ---------------------
+      Command functions
+    ---------------------
+]]
+
+function pgs:default()
+    local sessionTime  = getSessionTime()
+    local sessionMoney = getSessionMoney()
+    printToChat(strings.session.title)
+    printToChat(strings.session.sessionTime, hmsToString(sessionTime))
+    printToChat(
+        strings.session.moneyTotal,
+        gscToString(sessionMoney),
+        gscToString(moneyPerHour(sessionMoney, sessionTime))
+    )
+end
+
+function pgs:help(msg)
+    for v in arrayValues(strings["help"]) do
+        printToChat(v)
+    end
+end
+
+--[[
+function pgs:hour()
+    local time_diff = GetTime() - self.start_time;
+    if time_diff >= (3600) then
+        local xp_per_hour = (self.xp_gained / time_diff) * 3600;
+        printToChat("XP per hour: "..xp_per_hour);
     else
-        DEFAULT_CHAT_FRAME:AddMessage(strings["instance"].invalid);
+        printToChat("You have not been logged in without a reset for an hour");
+        printToChat("Time Logged IN: "..xp_util.to_hms_string(time_diff));
+    end
+end --TODO
+]]
+
+function pgs:instance()
+    tryUpdateInstanceInfo()
+    local instanceTitle = string.format(strings.instance.title,
+        instance.name,
+        instance.difficultyName,
+        instance.inProgress and strings.instance.inProgress or strings.instance.done
+    )
+
+    if instance.isValid then
+        local moneyTotal = getInstanceMoney()
+        local timeTotal  = getInstanceTime()
+        printToChat(instanceTitle);
+        printToChat(
+            strings["instance"].info,
+            hmsToString(timeTotal),
+            gscToString(moneyTotal),
+            gscToString(moneyPerHour(moneyTotal, timeTotal))
+        )
+    else
+        printToChat(strings["instance"].invalid)
     end
 end
 
 function pgs:reset()
-    session.startMoney = GetMoney();
-    session.startTime  = GetTime();
-    DEFAULT_CHAT_FRAME:AddMessage(strings["info"].RESET);
+    reset()
+    printToChat(strings["info"].RESET)
+end
+
+
+
+--[[
+    ---------------------
+      Exported functions
+    ---------------------
+]]
+function PiniGS:session()
+    local _time  = getSessionTime()
+    local _money = getSessionMoney()
+    return {
+        time  = _time,
+        money = _money,
+        moneyPerHour = moneyPerHour(_money, _time)
+    }
+end
+
+function PiniGS:instance()
+    tryUpdateInstanceInfo()
+    local _time = 0
+    local _money = 0
+    
+    if instance.isValid then
+        _time  = getInstanceTime()
+        _money = getInstanceMoney()
+    end
+
+    _moneyPerHour = moneyPerHour(_money, _time)
+    _time = hmsToString(_time)
+
+    return {
+        inProgress = instance.inProgress,
+        name = instance.name,
+        difficultyName = instance.difficultyName,
+        time  = _time,
+        money = _money,
+        moneyPerHour = _moneyPerHour
+    }
 end
---
+++ elvUI/datatexts/goldStats.lua
@@ -0,0 +1,149 @@
+if (IsAddOnLoaded("ElvUI")) then
+
+    local E, L, V, P, G = unpack(ElvUI); --Inport: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
+    local DT = E:GetModule('DataTexts')
+    local format = format
+
+    local enteredFrame = false
+    local initialUpdateTimer = 5
+    local updateTimer = initialUpdateTimer
+
+    local text = {
+        PLUS = "|cFF00FF00+ |r",
+        NEG  = "|cFFFF0000- |r",
+
+        PLUS_P_H = "|cFF00FF00 / h|r",
+        ZERO_P_H = "|r / h|r",
+        NEG_P_H  = "|cFFFF0000 / h|r",
+
+        MAIN_TITLE = "Pini - GoldStats",
+
+        PER_HOUR     = "Per hour:",
+        EARNED       = "Earned:",
+        DEFICIT      = "Deficit:",
+        STATUS       = "Status:",
+        NAME         = "Name:",
+        DIFFICULTY   = "Difficulty:",
+        RUN_TIME     = "Run time:",
+        
+        SESSION  = "Session:",
+        INSTANCE = "Instance:",
+
+        INSTANCE_DONE = "Done",
+        INSTANCE_IN_PROGRESS = "In progress..."
+    }
+
+    local function onEvent(self, event, ...)
+        local textOnly = not E.db.datatexts.goldCoins and true or false
+        local style = E.db.datatexts.goldFormat or 'BLIZZARD'
+        local session = PiniGS.session()
+
+        if session.moneyPerHour > 0 then
+            self.text:SetText(text.PLUS .. E:FormatMoney(session.moneyPerHour, style, textOnly) .. text.PLUS_P_H)
+        elseif session.moneyPerHour < 0 then
+            self.text:SetText(text.NEG .. E:FormatMoney(session.moneyPerHour, style, textOnly) .. text.NEG_P_H)
+        else
+            self.text:SetText(E:FormatMoney(session.moneyPerHour, style, textOnly) .. text.ZERO_P_H)
+        end
+    end
+
+    local function onEnter(self)
+        local session  = PiniGS.session()
+        local instance = PiniGS.instance()
+
+        enteredFrame = true
+
+        DT:SetupTooltip(self)
+
+        local textOnly = not E.db.datatexts.goldCoins and true or false
+        local style = E.db.datatexts.goldFormat or 'BLIZZARD'
+
+        DT.tooltip:AddLine(L[text.MAIN_TITLE])
+        DT.tooltip:AddLine(" ")
+
+        --[[ ---- SESSION ---- ]]
+        DT.tooltip:AddLine(L[text.SESSION])
+
+        if session.money >= 0 then
+            DT.tooltip:AddDoubleLine(L[text.EARNED], E:FormatMoney(session.money, style, textOnly), 1, 1, 1, 1, 1, 1)
+        else
+            DT.tooltip:AddDoubleLine(L[text.DEFICIT], E:FormatMoney(session.money, style, textOnly), 1, 0, 0, 1, 1, 1)
+        end
+
+        if session.moneyPerHour > 0 then
+            DT.tooltip:AddDoubleLine(L[text.PER_HOUR], E:FormatMoney(session.moneyPerHour, style, textOnly), 0, 1, 0, 1, 1, 1)
+        elseif session.moneyPerHour < 0 then
+            DT.tooltip:AddDoubleLine(L[text.PER_HOUR], "- " .. E:FormatMoney(session.moneyPerHour, style, textOnly), 1, 0, 0, 1, 1, 1)
+        else
+            DT.tooltip:AddDoubleLine(L[text.PER_HOUR], E:FormatMoney(session.moneyPerHour, style, textOnly), 1, 1, 1, 1, 1, 1)
+        end
+
+        DT.tooltip:AddLine(" ")
+
+        --[[ ---- INSTANCE ---- ]]
+        DT.tooltip:AddLine(L[text.INSTANCE])
+        if instance.inProgress then
+            DT.tooltip:AddDoubleLine(L[text.STATUS], text.INSTANCE_IN_PROGRESS, 1, 1, 1, 1, 0, 1)
+        else
+            DT.tooltip:AddDoubleLine(L[text.STATUS], text.INSTANCE_DONE, 1, 1, 1, 0, 1, 0)
+        end
+        DT.tooltip:AddDoubleLine(L[text.NAME], instance.name, 1, 1, 1, 0, 1, 1)
+        DT.tooltip:AddDoubleLine(L[text.DIFFICULTY], instance.difficultyName, 1, 1, 1, 1, 1, 1)
+        DT.tooltip:AddDoubleLine(L[text.RUN_TIME], instance.time, 1, 1, 1, 1, 1, 1)
+
+        if instance.money >= 0 then
+            DT.tooltip:AddDoubleLine(L[text.EARNED], E:FormatMoney(instance.money, style, textOnly), 1, 1, 1, 1, 1, 1)
+        else
+            DT.tooltip:AddDoubleLine(L[text.DEFICIT], E:FormatMoney(instance.money, style, textOnly), 1, 0, 0, 1, 1, 1)
+        end
+
+        if instance.moneyPerHour > 0 then
+            DT.tooltip:AddDoubleLine(L[text.PER_HOUR], E:FormatMoney(instance.moneyPerHour, style, textOnly), 0, 1, 0, 1, 1, 1)
+        elseif session.moneyPerHour < 0 then
+            DT.tooltip:AddDoubleLine(L[text.PER_HOUR], "- " .. E:FormatMoney(instance.moneyPerHour, style, textOnly), 1, 0, 0, 1, 1, 1)
+        else
+            DT.tooltip:AddDoubleLine(L[text.PER_HOUR], E:FormatMoney(instance.moneyPerHour, style, textOnly), 1, 1, 1, 1, 1, 1)
+        end
+
+        DT.tooltip:AddLine(" ")
+        DT.tooltip:Show()
+    end
+
+    local function onLeave()
+        enteredFrame = false
+        DT.tooltip:Hide()
+    end
+
+    local function update(self, frametime)
+        updateTimer = updateTimer - frametime
+
+        if updateTimer < 0 then
+            local textOnly = not E.db.datatexts.goldCoins and true or false
+            local style = E.db.datatexts.goldFormat or 'BLIZZARD'
+            local session = PiniGS.session()
+
+            if session.moneyPerHour > 0 then
+                self.text:SetText(text.PLUS .. E:FormatMoney(session.moneyPerHour, style, textOnly) .. text.PLUS_P_H)
+            elseif session.moneyPerHour < 0 then
+                self.text:SetText(text.NEG .. E:FormatMoney(session.moneyPerHour, style, textOnly) .. text.NEG_P_H)
+            else
+                self.text:SetText(E:FormatMoney(session.moneyPerHour, style, textOnly) .. text.ZERO_P_H)
+            end
+
+            updateTimer = initialUpdateTimer
+        end
+    end
+        
+    --[[
+        DT:RegisterDatatext(name, events, eventFunc, updateFunc, clickFunc, onEnterFunc, onLeaveFunc)
+        
+        name - name of the datatext (required)
+        events - must be a table with string values of event names to register 
+        eventFunc - function that gets fired when an event gets triggered
+        updateFunc - onUpdate script target function
+        click - function to fire when clicking the datatext
+        onEnterFunc - function to fire OnEnter
+        onLeaveFunc - function to fire OnLeave, if not provided one will be set for you that hides the tooltip.
+    ]]
+    DT:RegisterDatatext('PGS - session gold/h', {'PLAYER_ENTERING_WORLD', 'PLAYER_MONEY'}, onEvent, update, nil, onEnter, onLeave)
+end
