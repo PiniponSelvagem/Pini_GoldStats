3bd3ff5cd53af94f9591ef02dd17d62bb43c7340
PiniponSelvagem <piniponselvagem@gmail.com>
2020-07-17 17:01:49 +0100

    Fixed ElvUI datatext crash with newest ElvUI version.

--- elvUI/datatexts/goldStats.lua
+++ elvUI/datatexts/goldStats.lua
@@ -138,14 +138,14 @@ if (IsAddOnLoaded("ElvUI")) then
         
     --[[
         DT:RegisterDatatext(name, events, eventFunc, updateFunc, clickFunc, onEnterFunc, onLeaveFunc)
-        
+
         name - name of the datatext (required)
-        events - must be a table with string values of event names to register 
+        events - must be a table with string values of event names to register
         eventFunc - function that gets fired when an event gets triggered
         updateFunc - onUpdate script target function
         click - function to fire when clicking the datatext
         onEnterFunc - function to fire OnEnter
         onLeaveFunc - function to fire OnLeave, if not provided one will be set for you that hides the tooltip.
     ]]
-    DT:RegisterDatatext('PGS - session gold/h', {'PLAYER_ENTERING_WORLD', 'PLAYER_MONEY'}, onEvent, update, nil, onEnter, onLeave)
+    DT:RegisterDatatext('PGS - session gold/h', nil, {"PLAYER_ENTERING_WORLD", "PLAYER_MONEY"}, onEvent, update, nil, onEnter, onLeave)
 end
